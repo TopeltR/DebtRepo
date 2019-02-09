5107c3fa698b25d55783015b6558bb4a5b16e5d0
Liine Kasak <liine.kasak@gmail.com>
2019-02-09 20:00:00 +0200

    #6 userRepository and registering user test :sweat_drops:

--- src/main/java/ee/taltech/debty/controller/UserController.java
+++ src/main/java/ee/taltech/debty/controller/UserController.java
@@ -2,13 +2,26 @@ package ee.taltech.debty.controller;
 
 import ee.taltech.debty.entity.BankAccount;
 import ee.taltech.debty.entity.User;
+import ee.taltech.debty.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/users")
     List<User> getUsers() {
         BankAccount bankAccount = new BankAccount();
@@ -19,4 +32,10 @@ public class UserController {
 
         return List.of(user);
     }
+
+    @PostMapping("/users/register")
+    User registerUser(@Validated @RequestBody User user) {
+        return userService.saveUser(user);
+    }
+
 }
--- src/main/java/ee/taltech/debty/repository/UserRepository.java
+++ src/main/java/ee/taltech/debty/repository/UserRepository.java
@@ -2,6 +2,8 @@ package ee.taltech.debty.repository;
 
 import ee.taltech.debty.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
