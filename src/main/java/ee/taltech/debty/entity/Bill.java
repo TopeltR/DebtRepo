201212a5c21a0a6e8a4981be9409585c89c9ac89
Liine Kasak <liine.kasak@gmail.com>
2019-02-09 19:21:12 +0200

    #3 Bill entity

---
+++ src/main/java/ee/taltech/debty/entity/Bill.java
@@ -0,0 +1,23 @@
+package ee.taltech.debty.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.util.List;
+
+@Data
+@Entity
+public class Bill {
+    @Id
+    Long id;
+    String title;
+    String description;
+    User buyer;
+    List<User> users;
+    BigDecimal sum;
+    Currency currency;
+    List<BillPayment> billPayments;
+}
