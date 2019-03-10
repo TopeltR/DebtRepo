4d8c6abf655a938c10d50657c6b1b0b50a102107
mliibe <mliibe@ttu.ee>
2019-03-10 16:37:06 +0200

    BillService saveBill

--- src/main/java/ee/taltech/debty/conf/WebConfig.java
+++ src/main/java/ee/taltech/debty/conf/WebConfig.java
@@ -10,6 +10,7 @@ public class WebConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
+
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**");
