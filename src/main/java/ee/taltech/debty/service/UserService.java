6c94a3214a3ef72281acc141acd5b8e60f162c3f
Rasmus <rarung@ttu.ee>
2019-03-17 14:19:31 +0200

    Liine is REEEEEEEEEEE :fire:

--- src/main/java/ee/taltech/debty/service/UserService.java
+++ src/main/java/ee/taltech/debty/service/UserService.java
@@ -48,42 +48,5 @@ public class UserService {
         return personRepository.findByEmail(email) != null;
     }
 
-   /* public List<Person> getAllContactsById(Long id) {
-        return personRepository.findById(id).isPresent() ? personRepository.findById(id).get().getContacts()
-                : new ArrayList<>();
-    }
 
-    public List<Person> addContactToUserById(Long person, Long contact) {
-        Person p = new Person();
-        Person f = new Person();
-        if (personRepository.findById(person).isPresent()) p = personRepository.findById(person).get();
-        if (personRepository.findById(contact).isPresent()) f = personRepository.findById(contact).get();
-        p.getContacts().add(f);
-        personRepository.save(p);
-        return p.getContacts();
-    }
-
-
-
-    public void removeContactById(Long from_id, Long contact_id) {
-        if (personRepository.findById(from_id).isPresent() && personRepository.findById(contact_id).isPresent()) {
-            Person person = personRepository.findById(from_id).get();
-            Person from = personRepository.findById(contact_id).get();
-            person.getContacts().remove(from);
-            personRepository.save(person);
-        }
-    }
-
-    public List<Person> getAddableContacts(Long id) {
-        List<Person> newContacts = new ArrayList<>();
-        Optional<Person> personOptional = getUserById(id);
-        if (personOptional.isPresent()) {
-            Person person = personOptional.get();
-            List<Person> friends = person.getContacts();
-            List<Person> people = getAllUsers();
-            newContacts = people.stream().filter(p -> !friends.contains(p)).collect(Collectors.toList());
-            newContacts.remove(person);
-        }
-        return newContacts;
-    } */
 }
