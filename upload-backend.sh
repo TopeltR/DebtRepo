18e2fc6c2df7c4ac754bfa1e7cc38c5747503286
ingmar <ingmar@liibert.ee>
2019-04-18 10:32:32 +0300

    gradle deploy task

--- upload-backend.sh
+++ upload-backend.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-scp -i Desktop/aws.pem IdeaProjects/debty-api/build/libs/debty-0.0.1-SNAPSHOT.jar ubuntu@ec2-3-93-154-97.compute-1.amazonaws.com:/home/ubuntu
+scp -i /home/ingmar/Downloads/aws.pem /home/ingmar/Documents/Kool/4.\ semester/Tarkvaratehnika/debty-api/build/libs/debty-0.0.1-SNAPSHOT.jar ubuntu@ec2-3-93-154-97.compute-1.amazonaws.com:/home/ubuntu
