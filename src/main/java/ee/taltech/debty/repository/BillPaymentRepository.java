6eabe218db66a5bd87865d618a5ed7ea34773492
LiineKasak <liine.kasak@gmail.com>
2019-04-25 10:47:12 +0300

    try fix validation errors

--- src/main/java/ee/taltech/debty/repository/BillPaymentRepository.java
+++ src/main/java/ee/taltech/debty/repository/BillPaymentRepository.java
@@ -2,8 +2,10 @@ package ee.taltech.debty.repository;
 
 import ee.taltech.debty.entity.BillPayment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RepositoryRestResource(exported = false)
 public interface BillPaymentRepository extends JpaRepository<BillPayment, Long> {
 }
--- src/main/java/ee/taltech/debty/repository/ContactRepository.java
+++ src/main/java/ee/taltech/debty/repository/ContactRepository.java
@@ -3,12 +3,14 @@ package ee.taltech.debty.repository;
 import ee.taltech.debty.entity.Contact;
 import ee.taltech.debty.entity.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
+@RepositoryRestResource(exported = false)
 public interface ContactRepository extends JpaRepository <Contact, Long> {
 
     List<Contact> findAllByTo(Person person);
